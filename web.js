8cf2ad4d5052f735c032c82f9e4ae52622888c90
Ubuntu <ubuntu@ip-172-31-24-71.us-west-2.compute.internal>
2013-07-10 22:40:09 +0000

    another try 2

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var index_html = fs.readFileSync("index.html");
     var buf = new Buffer(index_html);
-    for (var i = 0; i < fileName.length; i++){
+    for (var i = 0; i < index_html.length; i++){
 	buf[i] = str.charCodeA(i);
 	}
     response.send(buf.toString());
